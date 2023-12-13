60440bed874248a1581b1a44477ed6fea14faa15
LionelMv <mvangicode@gmail.com>
2023-12-14 02:15:15 +0300

    Update context dictionary for posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+
 
 def home(request):
-    context = {"title": "Home"}
+    """Home page route.
+    """
+    context = {
+        "title": "Home",
+        "posts": Post.objects.all()
+    }
     return render(request, "blog/home.html", context)
