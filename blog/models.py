c8fefc40a3e28e2323b247e43beb2286947b57e8
LionelMv <mvangicode@gmail.com>
2023-12-14 02:13:00 +0300

    Add Post model and configurations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """Creates the post fields.
+    """
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        """String representation of the class Post.
+        """
+        return self.title
