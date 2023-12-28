b75d0e2a276c95d41dc3cefcc2f66376c8e5d83a
LionelMv <mvangicode@gmail.com>
2023-12-28 11:12:43 +0300

    Add users url path

--- TradersIn/urls.py
+++ TradersIn/urls.py
@@ -17,8 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
-    path("register/", include("users.urls")),
-    path("", include("blog.urls")),
     path('admin/', admin.site.urls),
+    path("", include("blog.urls")),
+    path("accounts/", include("users.urls")),
 ]
