b6b847706698cbc28535dd3aa68b627cbd6286b5
LionelMv <mvangicode@gmail.com>
2023-12-28 11:22:56 +0300

    Change message for toast.
    Change redirect url
    Add profile view

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import UserRegisterForm
 
@@ -9,9 +10,11 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username}")
-            return redirect("blog-home")
+            # username = form.cleaned_data.get("username")
+            messages.success(request,
+                             f"Your account has been created. \
+                             You can now login.")
+            return redirect("login")
     else:
         form = UserRegisterForm()
 
@@ -20,3 +23,8 @@ def register(request):
         "form": form
     }
     return render(request, "users/register.html", context)
+
+
+@login_required
+def profile(request):
+    return render(request, "users/profile.html", {"title": "Profile"})
