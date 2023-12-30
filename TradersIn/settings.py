d32b30919a76215516fafe2595185df64d0f4938
LionelMv <mvangicode@gmail.com>
2023-12-30 11:58:42 +0300

    Add MEDIA_ROOT and MEDIA_URL

--- TradersIn/settings.py
+++ TradersIn/settings.py
@@ -127,6 +127,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Uploaded pics directory
+MEDIA_ROOT = BASE_DIR.joinpath('media')
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
