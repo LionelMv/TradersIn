d6c96831217a58b4da265a9a4b6ac8341551ba57
LionelMv <mvangicode@gmail.com>
2024-07-09 13:25:45 +0300

    feat: Add register_user function in views.py
    Added two methods to manage registration of a user:
    - redirect_authenticated_user(): ensures logout of current user is
      required to view the registered page. Applies when a user goes to this
    url when there is a logged in user.
    
    - register_user(): manages the registration of the user details.

--- user/views.py
+++ user/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from .forms import SignUpForm
+from functools import wraps
 
-# Create your views here.
+
+def redirect_authenticated_user(view_func):
+    """Redirect authenticated user to home page."""
+    @wraps(view_func)
+    def _wrapped_view(request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.success(request,
+                             "Logout current user to access this page!")
+            return redirect("blog:home")
+        return view_func(request, *args, **kwargs)
+    return _wrapped_view
+
+
+@redirect_authenticated_user
+def register_user(request):
+    """Register user page."""
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # clean data
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # Login user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+
+            # create profile for user
+            # profile = Profile.objects.create(user=user)
+            messages.success(request,
+                             f"Your account has been created. \
+                             Welcome!.")
+            # redirect to home page after registration
+            return redirect("blog:home")
+    else:
+        form = SignUpForm()
+
+    context = {
+        "title": "SignUp",
+        "form": form
+    }
+    return render(request, "user/register.html", context)
