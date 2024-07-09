b1cb2a92d275d61a4c7f2fca2c4f04340e800125
LionelMv <mvangicode@gmail.com>
2024-07-09 10:58:09 +0300

    feat: Add home view function to views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def home(request):
+    """Home page route."""
+    context = {
+        "title": "Home",
+        "posts": Post.objects.all().order_by("-date_posted"),
+    }
+    return render(request, "blog/home.html", context)
