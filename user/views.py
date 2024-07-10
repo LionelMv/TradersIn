85ddb7cfac1a65370a338f0fe82daf21da4d0b58
LionelMv <mvangicode@gmail.com>
2024-07-10 16:09:00 +0300

    feat: Add update_user view to update user profile
    Added update_user function that will allow users to update their
    profile. Uses UserUpdateForm and ProfilePictureForm from forms.py.

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from .forms import SignUpForm
+from .forms import SignUpForm, UserUpdateForm, ProfilePictureForm
 from functools import wraps
 from .models import Profile
 from blog.models import Post
@@ -90,3 +90,36 @@ def profile(request, pk):
         "posts": posts
     }
     return render(request, "user/profile.html", context)
+
+
+@login_required(login_url="users:login")
+def update_user(request):
+    """Update user profile page."""
+    if request.method == "POST":
+        user_form = UserUpdateForm(
+            request.POST,
+            instance=request.user
+            )
+        pic_form = ProfilePictureForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile
+            )
+        if user_form.is_valid() and pic_form.is_valid():
+            user_form.save()
+            pic_form.save()
+            # login(request, request.user)
+            messages.success(request, "Your account has been updated!")
+            return redirect("user:profile", pk=request.user.id)
+
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        pic_form = ProfilePictureForm(instance=request.user.profile)
+
+    context = {
+        "title": "Update Profile",
+        "user_form": user_form,
+        "pic_form": pic_form
+    }
+
+    return render(request, "user/update_user.html", context)
