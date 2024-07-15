3e4179fb4a07c24e82c1f70e175b711d43978844
LionelMv <mvangicode@gmail.com>
2024-07-15 12:12:11 +0300

    feat: Create PostListView class to present home page
    Commented out the home view function and created a PostListView function
    to let Django handle the home view.
    It shows posts in the home page.

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 from .models import Post
 
 
-def home(request):
-    """Home page route."""
-    context = {
-        "title": "Home",
-        "posts": Post.objects.all().order_by("-date_posted"),
-    }
-    return render(request, "blog/home.html", context)
+# def home(request):
+#     """Home page route."""
+#     context = {
+#         "title": "Home",
+#         "posts": Post.objects.all().order_by("-date_posted"),
+#     }
+#     return render(request, "blog/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = "posts"
+    ordering = ["-date_posted"]
+    # paginate_by = 5
