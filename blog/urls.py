c15e28e2465f380ae7a1368dc9eacd3f3de6109c
LionelMv <mvangicode@gmail.com>
2024-07-15 12:54:11 +0300

    feat: Add path url to PostDetailView
    Added the url path to the PostDetailView that shows each individual
    post.

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import PostListView
+from .views import (
+    PostListView,
+    PostDetailView
+)
 
 
 app_name = "blog"
 urlpatterns = [
     path("", PostListView.as_view(), name="home"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
 ]
