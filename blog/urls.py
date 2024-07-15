f41b60de65e8b79f85dba1a1021c42a54f7d614f
LionelMv <mvangicode@gmail.com>
2024-07-15 12:16:04 +0300

    feat: Change path to home page to use PostListView
    Changed the url path of the home page from the function-based view i.e.
    home to class-based view i.e. PostListView

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
+from .views import PostListView
 
-from . import views
 
 app_name = "blog"
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("", PostListView.as_view(), name="home"),
 ]
