248913b563f33213b0cf1efc86e53a7f567c2ad5
LionelMv <mvangicode@gmail.com>
2024-07-16 14:36:55 +0300

    feat: Add post_update url for the PostUpdateView
    Added the post_update url path to cater for the PostUpdateView:
    "post/<int:pk>/update/"

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (
     PostListView,
     PostDetailView,
-    PostCreateView
+    PostCreateView,
+    PostUpdateView
 )
 
 
@@ -11,4 +12,5 @@ urlpatterns = [
     path("", PostListView.as_view(), name="home"),
     path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
     path("post/new/", PostCreateView.as_view(), name="post_create"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
 ]
