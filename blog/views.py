2c2478d54b4158b15ae95c7995ebc5ec7ade4844
LionelMv <mvangicode@gmail.com>
2024-07-16 14:34:15 +0300

    feat: Create PostUpdateView class
    Created the PostUpdateView class to enable users to be able to update
    their posts.
    Requires login and also requires that the author of the post is the only
    that can update the post. Uses these mixins for that:
    - LoginRequiredMixin
    - UserPassesTestMixin and a test_func() method.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView
 )
 from .models import Post
 
@@ -40,3 +41,21 @@ class PostCreateView(LoginRequiredMixin, CreateView):
         """Override form_valid method."""
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """Post update view."""
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        """Override form_valid method."""
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        """Override test_func method."""
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
