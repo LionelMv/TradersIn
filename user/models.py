5b16b8f5601a02f8b18bd1c7475427049cb08f07
LionelMv <mvangicode@gmail.com>
2024-07-17 17:26:52 +0300

    feat: Add follows field to the Profile model
    Added follows field to the Profile model to manage followers and
    following for user profiles

--- user/models.py
+++ user/models.py
@@ -6,6 +6,10 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.CharField(max_length=200, blank=True)
     image = models.ImageField(default='default.png', upload_to='profile_pics')
+    follows = models.ManyToManyField('self',
+                                     related_name='followed_by',
+                                     symmetrical=False,
+                                     blank=True)
 
     def __str__(self):
         return f'{self.user.username} Profile'
