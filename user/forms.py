76fe2b25dec42e245ed145c7e906381029595b7f
LionelMv <mvangicode@gmail.com>
2024-07-19 12:50:50 +0300

    feat: Remove the BaseForm class in forms.py
    Removed the BaseForm class which specified how individual attributes
    should be handled and let the cripsy forms do all that work

--- user/forms.py
+++ user/forms.py
@@ -4,49 +4,36 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
 
 
-class BaseForm(forms.ModelForm):
-    """Base form to provide common styling for fields"""
-    def __init__(self, *args, **kwargs):
-        super(BaseForm, self).__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
-            field.widget.attrs['placeholder'] = field.label
-            field.label = ''
-            if isinstance(field, forms.EmailField):
-                field.widget.attrs['placeholder'] = 'E-mail Address'
-
-
-class SignUpForm(BaseForm, UserCreationForm):
-    """Form for creating new users with additional styling"""
+class SignUpForm(UserCreationForm):
+    """Form for creating new users"""
     email = forms.EmailField(label="Email")
     first_name = forms.CharField(label="First Name", max_length=100)
     last_name = forms.CharField(label="Last Name", max_length=100)
 
     class Meta:
+        """Form metadata"""
         model = User
         fields = ["username", "first_name", "last_name",
                   "email", "password1", "password2"]
 
-    def __init__(self, *args, **kwargs):
-        super(SignUpForm, self).__init__(*args, **kwargs)
-        # Additional customization if needed
 
-
-class UserUpdateForm(BaseForm):
-    """Form to update user details with consistent styling"""
+class UserUpdateForm(forms.ModelForm):
+    """Form to update user details"""
     email = forms.EmailField(label="Email")
     first_name = forms.CharField(label="First Name", max_length=100)
     last_name = forms.CharField(label="Last Name", max_length=100)
 
     class Meta:
+        """Form metadata"""
         model = User
         fields = ["username", "first_name", "last_name", "email"]
 
 
 class ProfilePictureForm(forms.ModelForm):
-    """Form to update profile image"""
+    """Form to update profile picture"""
     image = forms.ImageField(label="Profile Picture", required=False)
 
     class Meta:
+        """Form metadata"""
         model = Profile
         fields = ["image"]
