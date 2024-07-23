35f124956b5d101d99513ef1a55dd2c593e80a9d
LionelMv <mvangicode@gmail.com>
2024-07-23 18:32:40 +0300

    feat: Add path urls for UserList and UserDetail views

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import (
+    PostList,
+    PostDetail,
+    UserList,
+    UserDetail
+)
 
 urlpatterns = [
     path('posts/', PostList.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
+    path('users/', UserList.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetail.as_view(), name='user-detail')
 ]
