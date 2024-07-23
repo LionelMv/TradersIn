729e3a5426885aace6e54e116887900f2ab8dcb5
LionelMv <mvangicode@gmail.com>
2024-07-23 21:00:08 +0300

    feat: Add IsAdminOrSelf in UserList and UserDetail classes
    Added IsAdminOrSelf in UserList and UserDetail classes'
    permissions_classes. Also added destroy method to UserDetail class to
    disallow deletion of users.

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
-from .permissions import IsAuthorOrReadOnly
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from .permissions import IsAuthorOrReadOnly, IsAdminOrSelf
 from blog.models import Post
 from user.models import User
 from .serializers import PostSerializer, UserSerializer
+
 
 class PostList(generics.ListCreateAPIView):
     """Json representation of all posts"""
@@ -14,6 +16,7 @@ class PostList(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         """Post author is logged in user"""
         serializer.save(author=self.request.user)
+
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     """Json representation of a single post"""
@@ -26,11 +29,18 @@ class UserList(generics.ListCreateAPIView):
     """Json representation of all users"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsAdminOrSelf]
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     """Json representation of a single user"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsAdminOrSelf]
+
+    def destroy(self, request, *args, **kwargs):
+        """Disallow DELETE method by raising a 403 Forbidden error"""
+        return Response(
+            {'detail': 'Method "DELETE" not allowed.'},
+            status=status.HTTP_403_FORBIDDEN
+            )
