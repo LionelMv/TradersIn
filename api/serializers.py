49096cac66c3e5785a2c06abfa51415c75b4d87b
LionelMv <mvangicode@gmail.com>
2024-07-23 18:27:46 +0300

    feat: Create  UserSerializer class
    Create UserSerializer class to handle User model

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
 from blog.models import Post
+from user.models import User
+
 
 class PostSerializer(serializers.ModelSerializer):
+    """Convert Post models to json"""
     class Meta:
         model = Post
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Convert User models to json"""
+    class Meta:
+        model = User
+        fields = ["id", "username", "first_name", "last_name", "email"]
