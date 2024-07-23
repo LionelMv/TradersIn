6a57aa2f6bdc9cff67607d2a88fe5699df8ec8ab
LionelMv <mvangicode@gmail.com>
2024-07-23 13:29:18 +0300

    feat: Add follow and unfollow paths to urls.py
    Added follow and unfollow url paths to urlpatterns in urls.py

--- user/urls.py
+++ user/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path("logout/", views.logout_user, name="logout"),
     path("profile/<int:pk>/", views.profile, name="profile"),
     path("update_user/", views.update_user, name="update_user"),
+    path('follow/<int:pk>/', views.toggle_follow_user, {'action': 'follow'}, name='follow_user'),
+    path('unfollow/<int:pk>/', views.toggle_follow_user, {'action': 'unfollow'}, name='unfollow_user'),
 ]
