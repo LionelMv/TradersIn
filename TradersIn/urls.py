c751eea65f476516b62f67eaf731326fa693b46e
LionelMv <mvangicode@gmail.com>
2024-07-23 17:03:05 +0300

    feat: Add url path to api app

--- TradersIn/urls.py
+++ TradersIn/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('user/', include('user.urls')),
+    path('api/', include('api.urls')),
 ]
 
 if settings.DEBUG:
