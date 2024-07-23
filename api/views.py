29a7ac32a6c2a42fb6a91014fea7229042104539
LionelMv <mvangicode@gmail.com>
2024-07-23 17:00:41 +0300

    feat: Create PostList and PostDetail api
    Created PostList and PostDetail api in views.py file

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from blog.models import Post
+from .serializers import PostSerializer
 
-# Create your views here.
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
