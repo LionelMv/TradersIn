93e033af3331e8ca565e4b33981aeca0a509127e
LionelMv <mvangicode@gmail.com>
2024-07-23 18:59:32 +0300

    feat: Add order_by sort to PostList class
    Added order_by to sort posts by date posted from recent to last.

--- api/views.py
+++ api/views.py
@@ -5,23 +5,31 @@ from user.models import User
 from .serializers import PostSerializer, UserSerializer
 
 class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
+    """Json representation of all posts"""
+    queryset = Post.objects.all().order_by('-date_posted')
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def perform_create(self, serializer):
+        """Post author is logged in user"""
+        serializer.save(author=self.request.user)
+
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    """Json representation of a single post"""
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
 class UserList(generics.ListCreateAPIView):
+    """Json representation of all users"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    """Json representation of a single user"""
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
