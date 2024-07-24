3e276ae86e725ac29e44dde5d94bfe5cf38a0045
LionelMv <mvangicode@gmail.com>
2024-07-24 09:17:45 +0300

    feat: Add url paths for profile api views
    Added url paths for profile api views i.e.:
    - profiles/
    - profiles/<int:pk>/
    - profiles/<int:pk>/followers/
    - profiles/<int:pk>/following/

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,20 @@ from .views import (
     PostList,
     PostDetail,
     UserList,
-    UserDetail
+    UserDetail,
+    ProfileList,
+    ProfileDetail,
+    ProfileFollowers,
+    ProfileFollowing
 )
 
 urlpatterns = [
     path('posts/', PostList.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
     path('users/', UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', UserDetail.as_view(), name='user-detail')
+    path('users/<int:pk>/', UserDetail.as_view(), name='user-detail'),
+    path("profiles/", ProfileList.as_view(), name="profile-list"),
+    path("profiles/<int:pk>/", ProfileDetail.as_view(), name="profile-detail"),
+    path("profiles/<int:pk>/followers/", ProfileFollowers.as_view(), name="profile-followers"),
+    path("profiles/<int:pk>/following/", ProfileFollowing.as_view(), name="profile-following"),
 ]
